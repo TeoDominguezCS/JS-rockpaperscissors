ad2858df3a82424b4c1ba51c8d6c0ae633544c7d
Teo <70834248+TeoDominguezCS@users.noreply.github.com>
2024-01-15 12:34:51 -0800

    initial commit add computer coice func

---
+++ rocks.js
@@ -0,0 +1,13 @@
+function getComputerChoice(){
+    let random = Math.floor(Math.random() * 3) ;
+    switch(random){
+        case 0:
+            return "Rock";
+        case 1: 
+            return "Paper";
+        case 2: 
+            return "Scissors";
+        default:
+            return "Error";
+    }
+}
